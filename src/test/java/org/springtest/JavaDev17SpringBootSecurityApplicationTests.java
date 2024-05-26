5ca8600765aedf7e58c385b9c289b6639f82bc87
Dmitry Diachenko <44314830+Fenki93@users.noreply.github.com>
2024-05-26 21:05:54 +0200

    initial commit security

---
+++ src/test/java/org/springtest/JavaDev17SpringBootSecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package org.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class JavaDev17SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
